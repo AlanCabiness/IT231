8ea5ffc14b9cc41080f61408b5f53f55008205a4
Cabiness <ACABINE1@depaul.edu>
2017-03-29 16:19:06 -0500

    initial commit

---
+++ lab1/app/controllers/say_controller.rb
@@ -0,0 +1,10 @@
+class SayController < ApplicationController
+  def hello
+  end
+
+  def response1
+  end
+
+  def response2
+  end
+end
---
+++ lab1/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'say/hello'
+
+  get 'say/response1'
+
+  get 'say/response2'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
